1f3beebdea6b9283502fee2e59b03b980c28b006
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 11:01:51 -0700

    Adds migration to add token column to users table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,3 +116,4 @@ m3_tutorial.videos.create!({
 })
 
 User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password:  "password", role: :admin)
+User.create!(email: 'default@example.com', first_name: 'Default', last_name: 'Human', password:  "pass", role: :default)
