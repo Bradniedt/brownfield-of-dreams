82423e7c906a113fec9f6b28074b00e515ac56b8
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 13:58:23 -0700

    Adds conditional logic to controller and view to handle user not having a token.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @repos = Repo.find_all(current_user.token)
+    @repos = Repo.find_all(current_user.token) if current_user.token
   end
 
   def new
