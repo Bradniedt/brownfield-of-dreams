fd3e8e48d45dc9b95277b0c90f9c58027f68e4cd
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 14:20:12 -0700

    Changes tests using Repo.find_all method to guard github token.

--- spec/features/user/user_sees_github_repos_spec.rb
+++ spec/features/user/user_sees_github_repos_spec.rb
@@ -3,22 +3,24 @@ require 'rails_helper'
 describe 'As a default user' do
   describe 'when I log in' do
     it 'my dashboard should have a github section with repo names as links' do
-      user = create(:user, token: "8caaa533fb393a502d014a1df646d123588782ef")
-      visit '/'
+      VCR.use_cassette("github_service") do
+        user = create(:user, token: "hello")
+        visit '/'
 
-      click_on "Sign In"
-      expect(current_path).to eq(login_path)
+        click_on "Sign In"
+        expect(current_path).to eq(login_path)
 
-      fill_in 'session[email]', with: user.email
-      fill_in 'session[password]', with: user.password
+        fill_in 'session[email]', with: user.email
+        fill_in 'session[password]', with: user.password
 
-      click_on 'Log In'
+        click_on 'Log In'
 
-      expect(current_path).to eq(dashboard_path)
-      expect(page).to have_css(".repo", count: 5)
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_css(".repo", count: 5)
 
-      within(".github") do
-        expect(page).to have_link("little-shop")
+        within(".github") do
+          expect(page).to have_link("little-shop")
+        end
       end
     end
     it 'my dashboard should NOT have a github section with repo names as links' do
--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -22,11 +22,13 @@ describe Repo do
   end
   describe 'class methods' do
     it '.find_all' do
-      token = "8caaa533fb393a502d014a1df646d123588782ef"
-      repos = Repo.find_all(token)
+      VCR.use_cassette("github_service") do
+        token = "hello"
+        repos = Repo.find_all(token)
 
-      expect(repos.count).to eq(5)
-      expect(repos[0]).to be_an_instance_of Repo
+        expect(repos.count).to eq(5)
+        expect(repos[0]).to be_an_instance_of Repo
+      end
     end
   end
 end
