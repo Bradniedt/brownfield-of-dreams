1d381f7e2708e3879d3472c6800b4e1dab1526fb
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 11:23:36 -0700

    Adds new test for repo attributes.

--- spec/models/repositories_spec.rb
+++ spec/models/repositories_spec.rb
@@ -10,4 +10,14 @@ describe Repo do
 
     expect(repo).to be_an_instance_of Repo
   end
+  it 'has attributes' do
+    data = {
+      name: "Repo1",
+      url: "www.hello.com"
+    }
+    repo = Repo.new(data)
+
+    expect(repo.name).to eq(data[:name])
+    expect(repo.url).to eq(data[:url])
+  end
 end
