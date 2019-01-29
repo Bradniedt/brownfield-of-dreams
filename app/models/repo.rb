095b53cf8b6138879784a3811f0426c7ff29c7da
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 13:36:09 -0700

    Adds find all method to repo model to pass repo model test.

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,6 +2,19 @@ class Repo
   attr_reader :name, :url
   def initialize(data)
     @name = data[:name]
-    @url = data[:url]
+    @url = data[:html_url]
+  end
+
+  def self.find_all(token)
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = "token #{token}"
+      faraday.headers["Accept"] = "application/vnd.github.v3+json"
+      faraday.adapter Faraday.default_adapter
+    end
+    response = @conn.get("/user/repos")
+    raw_repos = JSON.parse(response.body, symbolize_names: true).take(5)
+    raw_repos.map do |data|
+      Repo.new(data)
+    end
   end
 end
