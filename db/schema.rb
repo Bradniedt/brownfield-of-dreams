c14fa134c0730605244a0aadddf2388dd97931a9
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-29 15:04:31 -0700

    Removes duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_01_29_192319) do
+ActiveRecord::Schema.define(version: 2019_01_29_175929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
