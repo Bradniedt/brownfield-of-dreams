0aca2f3e0f286747bca87c57307a1f899b15b79b
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 14:50:48 -0700

    Adds new github service to pass github service tests.

---
+++ app/services/github_service.rb
@@ -0,0 +1,23 @@
+class GithubService
+  def initialize(token)
+    @token = token
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = token
+      faraday.headers["Accept"] = "application/vnd.github.v3+json"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_url(url)
+    response = @conn.get(url)
+    JSON.parse(response.body, symbolize_names: true).take(5)
+  end
+
+  def find_repos
+    get_url("/user/repos")
+  end
+
+  def self.find_repos(token)
+    new(token).find_repos
+  end
+end
