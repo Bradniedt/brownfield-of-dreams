3f5e46049c666e0246f8a13e9c2b05dbbf25ab80
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 20:15:57 -0700

    Adds github followers service code.

---
+++ app/services/github_follower_service.rb
@@ -0,0 +1,23 @@
+class GithubFollowerService
+  def initialize(token)
+    @token = token
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = ENV['GITHUB_API_KEY']
+      faraday.headers["Accept"] = "application/vnd.github.v3+json"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_url(url)
+    response = @conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def find_followers
+    get_url("/user/followers")
+  end
+
+  def self.find_followers(token)
+    new(token).find_followers
+  end
+end
