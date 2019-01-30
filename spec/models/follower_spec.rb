289fba6f1f3117dffa6f176706eb2f89244ccde3
Brad Niedt <bradleyniedt@gmail.com>
2019-01-29 20:11:32 -0700

    Adds small changes to follower model spec.

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Follower do
   it 'exists' do
     data = {
-      name: "Repo1",
-      html_url: "www.hello.com"
+      login: "Name1",
+      url: "www.hello.com"
     }
     follow = Follower.new(data)
 
@@ -12,23 +12,23 @@ describe Follower do
   end
   it 'has attributes' do
     data = {
-      name: "Repo1",
+      login: "Repo1",
       url: "www.hello.com"
     }
-    repo = Repo.new(data)
+    follow = Follower.new(data)
 
-    expect(repo.name).to eq(data[:name])
-    expect(repo.url).to eq(data[:url])
+    expect(follow.name).to eq(data[:login])
+    expect(follow.url).to eq(data[:url])
   end
   describe 'class methods' do
     it '.find_all' do
-      #VCR.use_cassette("github_follower_service") do
+      VCR.use_cassette("github_follower_service") do
         token = "hello"
         followers = Follower.find_all(token)
 
         expect(followers.count).to eq(11)
         expect(followers[0]).to be_an_instance_of Follower
-      #end
+      end
     end
   end
 end
