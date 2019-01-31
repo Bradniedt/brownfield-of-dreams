6bd01dcd789753c3ad31703f089b89e222401277
Brad Niedt <bradleyniedt@gmail.com>
2019-01-30 17:51:28 -0700

    Adds small change to github follower service.

--- app/services/github_follower_service.rb
+++ app/services/github_follower_service.rb
@@ -1,8 +1,8 @@
 class GithubFollowerService
   def initialize(token)
-    @token = ENV['GITHUB_API_KEY']
+    @token = token
     @conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["Authorization"] = @token
+      faraday.headers["Authorization"] = ENV['GITHUB_API_KEY']
       faraday.headers["Accept"] = "application/vnd.github.v3+json"
       faraday.adapter Faraday.default_adapter
     end
