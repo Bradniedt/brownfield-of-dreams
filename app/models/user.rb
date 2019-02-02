892e94477742633385c042b781a1787f1780a1a4
Brad Niedt <bradleyniedt@gmail.com>
2019-02-02 11:16:40 -0700

    Adds code to pass friendship model test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, if: :password
   validates_presence_of :first_name
