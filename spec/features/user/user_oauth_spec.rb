7891275ffa59fd9d6ea1042862f70a4bf8413aa9
Brad Niedt <bradleyniedt@gmail.com>
2019-02-02 08:51:35 -0700

    Adds test for omniauth.

--- spec/features/user/user_oauth_spec.rb
+++ spec/features/user/user_oauth_spec.rb
@@ -10,4 +10,19 @@ describe 'User Oauth' do
 
     expect(page).to have_button('Connect to Github')
   end
+  it 'omniauth provides token' do
+    VCR.use_cassette("oauth") do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        "credentials" => {"token" => ENV['GITHUB_API_KEY']}
+        })
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+      save_and_open_page
+
+      expect(page).to have_button('Connect to Github')
+    end
+  end
 end
