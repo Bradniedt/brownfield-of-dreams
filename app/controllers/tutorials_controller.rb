1333821f3f2a965b4d908d285d576139788e7f64
Brad Niedt <bradleyniedt@gmail.com>
2019-02-04 15:14:17 -0700

    Adds code to pass test.

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,8 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if !tutorial.videos.empty?
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 end
