354f24d7e7f423d6d641f2c0673291f1a46c04c2
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-05 15:17:14 -0700

    Adjusts test suite to accommodate the rest of card 7

--- spec/features/user/user_registration_spec.rb
+++ spec/features/user/user_registration_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'A visitor to our app' do
   it 'registration' do
+    clear_emails
     email = "fresh_baked_honeybun@healthyicing.gov"
     first_name = "Davoin"
     last_name = "Shower Handle"
@@ -21,30 +22,22 @@ describe 'A visitor to our app' do
 
     click_button "Create Account"
 
+
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content "Logged in as #{first_name}"
     expect(page).to have_content "This account has not yet been activated. Please check your email."
 
-    # When I check my email for the registration email
-    background do
-      clear_emails
-      visit email_trigger_path
-      open_email('fresh_baked_honeybun@healthyicing.gov')
-    end
-    
-    # I should see a message that says "Visit here to activate your account."
-    expect(current_email).to have_content "Visit here to activate your account."
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(User.find_by(email: "fresh_baked_honeybun@healthyicing.gov"))
 
-    # And when I click on that link
+    open_email('fresh_baked_honeybun@healthyicing.gov')
+    expect(current_email).to have_content "Visit this link to activate your account."
+
     current_email.click_link 'Activate'
 
-    # Then I should be taken to a page that says "Thank you! Your account is now activated."
-    expect(page).to have_content "Thank you! Your account is now activated."
+    expect(page).to have_content "Thank you! Your account is now activated!"
 
-    # And when I visit "/dashboard"
     visit dashboard_path
 
-    # Then I should see "Status: Active"
     expect(page).to have_content "Status: Active" #titlecase in the view or in controller.
 
   end
