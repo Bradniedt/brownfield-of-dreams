8cde28c38332a8465c2a781b5eaec3fcf1d3b002
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-05 13:50:34 -0700

    Adds flash if account is inactive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       flash[:notice] = "Logged in as #{user.first_name}"
+      unless user.active?
+        flash[:alert] = "This account has not yet been activated. Please check your email."
+      end
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
