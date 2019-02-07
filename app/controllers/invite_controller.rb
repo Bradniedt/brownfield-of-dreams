f82138cd63fba2cad115cdc99fa7051db866acce
Brad Niedt <bradleyniedt@gmail.com>
2019-02-07 10:44:13 -0700

    Added flash message for successfully sending invite and removed comment.

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,10 +3,10 @@ class InviteController < ApplicationController
   end
 
   def create
-    #maybe sanitize the github name here
     result = Email.find_address(current_user.token, params[:github_handle])
     if result.address.chars.include?("@")
       AccountNotifierMailer.invite(result.address, current_user, params[:github_handle]).deliver_now
+      flash[:notice] = "Successfully sent invite!"
     else
       flash[:notice] = "#{result.address}"
     end
