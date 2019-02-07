985cb2baea1dc321f14cb6057e0214915177c0e9
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-07 15:37:41 -0700

    Refactors significant code in controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,28 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to dashboard_path
-    else
-      flash[:error] = "Looks like your email or password is invalid"
-      render :new
-    end
+    valid(user) ? generate_session(user) : display_error
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def valid(user)
+    user && user.authenticate(params[:session][:password])
+  end
+
+  def generate_session(user)
+    session[:user_id] = user.id
+    redirect_to dashboard_path
+  end
+
+  def display_error
+    flash[:error] = "Looks like your email or password is invalid"
+    render :new
   end
 
 end
--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -4,12 +4,7 @@ class UserVideosController < ApplicationController
 
   def create
     user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-      flash[:error] = "Already in your bookmarks"
-    elsif user_video.save
-      flash[:success] = "Bookmark added to your dashboard!"
-    end
-
+    bookmark_exists(user_video) ? notify : confirm(user_video)
     redirect_back(fallback_location: root_path)
   end
 
@@ -17,5 +12,19 @@ class UserVideosController < ApplicationController
 
   def user_video_params
     params.permit(:user_id, :video_id)
+  end
+
+  def bookmark_exists(user_video)
+    return true if current_user.user_videos.find_by(video_id: user_video.video_id)
+  end
+
+  def notify
+    flash[:error] = "Already in your bookmarks"
+  end
+
+  def confirm(user_video)
+    if user_video.save
+      flash[:success] = "Bookmark added to your dashboard!"
+    end
   end
 end
