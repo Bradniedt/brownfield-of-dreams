dfe02cff6b81875e90f6d85becc67ad12421ca09
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 19:39:59 -0700

    Adds change to remove messages variable.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,19 +1,17 @@
 class Email
   attr_reader :address
   def initialize(data = {})
-    @address = set_address(data[:email])
-    @message = ""
+    @address = set_address(data)
   end
 
   def self.find_address(token, name)
     result = GithubEmailService.find_email(token, name)[:email]
-    binding.pry
     Email.new(result)
   end
 
   def set_address(result)
-    if !result.include?("@")
-      @message = "The Github user you selected doesn't have an email address associated with their account."
+    if result.nil?
+      "The Github user you selected doesn't have an email address associated with their account."
     else
       result
     end
